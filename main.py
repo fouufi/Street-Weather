c868e91b3b4ad7bc09227b3b2103c579c33fce20
Moha Specter <drame.moha44@gmail.com>
2020-06-01 17:00:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import requests
 import json 
 
 #http://api.openweathermap.org/data/2.5/weather?id=2990969&appid=4275cf39840e13bd2503187dce72cbab
+
+api_request = requests.get("http://api.openweathermap.org/data/2.5/weather?id=2990969&appid=4275cf39840e13bd2503187dce72cbab")
+try:
+    api = json.loads(api_request.content)
+except Exception as e
