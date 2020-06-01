9e5112f822caf677bd5ec981458381301f8b2db9
Moha Specter <drame.moha44@gmail.com>
2020-06-01 19:27:28 +0200

    erreur corrigée main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from tkinter import *
+import tkinter
 import requests 
 import json 
+from PIL import Image, ImageTk
 
-#http://api.openweathermap.org/data/2.5/weather?id=2990969&appid=4275cf39840e13bd2503187dce72cbab
+root = Tk()
+root.title('test titre')
+root.geometry("400x400")
 
 api_request = requests.get("http://api.openweathermap.org/data/2.5/weather?id=2990969&appid=4275cf39840e13bd2503187dce72cbab")
 try:
     api = json.loads(api_request.content)
-except Exception as e
+except Exception as e:
+    api = "Error"
+
+myLabel = Label(root, text=api)
+myLabel.pack()
