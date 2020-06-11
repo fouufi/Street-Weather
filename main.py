26e942ceb4bf42b782e3812628884de9b02ea6cd
mohaspecter <39928536+mohawakanda@users.noreply.github.com>
2020-06-11 17:43:10 +0200

    strings
    il manque les floats pour la récupération

--- main.py
+++ main.py
@@ -9,16 +9,39 @@ root.geometry("640x380")
 
 api_key = "http://api.openweathermap.org/data/2.5/weather?id=2990969&appid=4275cf39840e13bd2503187dce72cbab"
 api_request = requests.get(api_key)
-#x = api_request.jons()
 
 try:
     api = json.loads(api_request.content)
 except Exception as e:
     api = "Error"
 
+weather = json.dumps(api["weather"])
+print(weather)
 
-myLabel = Label(root, text=api, wraplength=580, bg='cyan')
-myLabel.pack()
+main = json.dumps(api["main"])
+print(main)
+
+clouds = json.dumps(api["clouds"])
+print(clouds)
+
+country = json.dumps(api["sys"]["country"])
+print(country)
+
+city = json.dumps(api["name"])
+print(city)
+
+
+
+myLabel1 = Label(root, text=api["weather"], wraplength=580, bg='cyan')
+myLabel2 = Label(root, text=api["main"], wraplength=580, bg='cyan')
+myLabel3 = Label(root, text=api["clouds"], wraplength=580, bg='cyan')
+myLabel4 = Label(root, text=api["sys"], wraplength=580, bg='cyan')
+myLabel5 = Label(root, text=api["name"], wraplength=580, bg='cyan')
+
+myLabel1.pack()
+myLabel2.pack()
+myLabel3.pack()
+myLabel4.pack()
+myLabel5.pack()
 
 root.mainloop()
-
