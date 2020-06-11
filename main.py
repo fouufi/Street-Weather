d25a04b9189a66ace911e0b185e84b746d9e458f
mohaspecter <39928536+mohawakanda@users.noreply.github.com>
2020-06-11 16:09:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-import tkinter
+from tkinter import *
 import requests 
 import json 
 from PIL import Image, ImageTk
 
 root = Tk()
-root.title('test titre')
-root.geometry("400x400")
+root.title('Street Weather')
+root.geometry("640x380")
 
-api_request = requests.get("http://api.openweathermap.org/data/2.5/weather?id=2990969&appid=4275cf39840e13bd2503187dce72cbab")
+api_key = "http://api.openweathermap.org/data/2.5/weather?id=2990969&appid=4275cf39840e13bd2503187dce72cbab"
+api_request = requests.get(api_key)
+#x = api_request.jons()
+
 try:
     api = json.loads(api_request.content)
 except Exception as e:
     api = "Error"
 
-myLabel = Label(root, text=api)
+
+myLabel = Label(root, text=api, wraplength=580, bg='cyan')
 myLabel.pack()
+
+root.mainloop()
+
