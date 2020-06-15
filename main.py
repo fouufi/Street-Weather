2785014dfa1ef86886fd0297e6ad7f0eb93389ba
mohaspecter <39928536+mohawakanda@users.noreply.github.com>
2020-06-15 18:58:01 +0200

    Update main.py
    infos rentrées dans la bdd

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from tkinter import *
 import requests 
 import json 
 from PIL import Image, ImageTk
+import mysql.connector as mariadb
+from mysql.connector import errorcode
 
 root = Tk()
 root.title('Street Weather')
@@ -42,6 +44,9 @@ print(country)
 city = json.dumps(api["name"])
 print(city)
 
+city_id = json.dumps(api["id"])
+print(city_id)
+
 myLabel1 = Label(root, text=api["weather"], wraplength=580, bg='cyan')
 myLabel2 = Label(root, text=api["main"], wraplength=580, bg='cyan')
 myLabel3 = Label(root, text=api["clouds"], wraplength=580, bg='cyan')
@@ -54,4 +59,44 @@ myLabel3.pack()
 myLabel4.pack()
 myLabel5.pack()
 
+mariadb_connection = mariadb.connect(user='root', password='1234', database='streetweather')
+cursor = mariadb_connection.cursor()
+cursor.execute("SET sql_mode = '' ")
+
+sqlvalues1 = ("INSERT INTO meteo" 
+                "( Temps, Description, Temperature, Temp_Ressentie, Humidite, Nuages)" 
+                "VALUES(%(Temps)s,%(Description)s,%(Temperature)s,%(Temp_Ressentie)s,%(Humidite)s,%(Nuages)s)")
+sqlvalue1 = {
+    'Temps':temps,
+    'Description':description,
+    'Temperature':temp,
+    'Temp_Ressentie':rtemp,
+    'Humidite':humidity,
+    'Nuages':clouds,
+    }
+cursor.execute(sqlvalues1,sqlvalue1)
+mariadb_connection.commit()
+
+sqlvalues2 = ("INSERT INTO pays" 
+                "(Pays)" 
+                "VALUES(%(Pays)s)")
+sqlvalue2={
+    'Pays':country,
+    }
+cursor.execute(sqlvalues2,sqlvalue2)
+mariadb_connection.commit()
+
+sqlvalues3 = ("INSERT INTO ville" 
+                "(ID_Ville,Ville)" 
+                "VALUES(%(ID_Ville)s,%(Ville)s)")
+sqlvalue3={
+    'ID_Ville':city_id,
+    'Ville':city,
+    }
+cursor.execute(sqlvalues3,sqlvalue3)
+mariadb_connection.commit()
+
 root.mainloop()
+
+cursor.close()
+mariadb_connection.close()
